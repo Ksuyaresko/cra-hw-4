bca4e6511c565754f82f046f4cb670e5c7cb4982
Ksuyaresko <kiaresko@gmail.com>
2019-11-27 09:45:29 +0200

    Updates

---
+++ precache-manifest.ca71ad552917aaff584e85a25b68e163.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "486c2a4c14dbf192079d12ec29315fc6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9be2f8fe172471786dc",
+    "url": "/static/css/main.b866c68d.chunk.css"
+  },
+  {
+    "revision": "f17327ecab4bcd7837f7",
+    "url": "/static/js/2.21b24cc3.chunk.js"
+  },
+  {
+    "revision": "f9be2f8fe172471786dc",
+    "url": "/static/js/main.34c1f799.chunk.js"
+  },
+  {
+    "revision": "abb500e6833f86a44f42",
+    "url": "/static/js/runtime-main.710010e6.js"
+  }
+]);
